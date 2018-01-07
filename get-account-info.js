2c4c1178b20f09f77131ff9f7138a4de78d62823
root <root@debian-work>
2018-01-07 17:24:35 -0300

    First Commit7

---
+++ get-account-info.js
@@ -0,0 +1,32 @@
+//This script, fetches information about a hard-coded account. Use it to test that RippleAPI works.
+
+'use strict';
+const RippleAPI = require('ripple-lib').RippleAPI; // Imports RippleAPI into the current scope using Node.js require function.  RippleAPI is one of the modules ripple-lib expoerts
+
+
+// Instantiating the API
+const api = new RippleAPI({
+    server: 'wss://s1.ripple.com'  // Public ripplled server
+});
+
+api.connect().then(()=>{
+  const myAddres='r3qQrJMkXW8WexLDgKrT6rPC8C6QMy9y4p';
+  console.log('Getting account info for', myAddres);
+  return api.getAccountInfo(myAddres)
+}).then(info=>{
+  console.log(info);
+  console.log('GetAccountInfoDone')
+})
+
+/*
+Output info:
+
+{
+  sequence: 6,
+  xrpBalance: '435.16216',
+  ownerCount: 1,
+  previousAffectingTransactionID: '9DC318489DB9A900A40CBE26EF84A1E6A11DB4E94CECB6CE27A7DA23259BF431',
+  previousAffectingTransactionLedgerVersion: 35624580
+}
+
+*/
