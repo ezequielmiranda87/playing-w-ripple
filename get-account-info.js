96c32bc19bec198c729fb7791bda2a77380c1112
root <root@debian-work>
2018-01-08 11:31:28 -0300

    Add Test Ripple Server

--- get-account-info.js
+++ get-account-info.js
@@ -3,30 +3,25 @@
 'use strict';
 const RippleAPI = require('ripple-lib').RippleAPI; // Imports RippleAPI into the current scope using Node.js require function.  RippleAPI is one of the modules ripple-lib expoerts
 
+const RippleServer = {server: 'wss://s1.ripple.com'};
+const testServer = {server:'wss://s.altnet.rippletest.net:51233'}
+
+
+// Test address  - balance: 10,000XRP
+const testAddress = {
+  public: 'rUGJAj3ErWY25W66CSF9EUhjdKFnumD7Lj',
+  secret: 'ssDvz84CArogDAHKkpLiZHcqZyC59'
+}
+
 
 // Instantiating the API
-const api = new RippleAPI({
-    server: 'wss://s1.ripple.com'  // Public ripplled server
-});
+const api = new RippleAPI(testServer)  // Public ripplled server);
 
 api.connect().then(()=>{
   const myAddres='rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn';
-  console.log('Getting account info for', myAddres);
-  return api.getAccountInfo(myAddres)
-}).then(info=>{
+  console.log('Getting account info for', testAddress.public);
+  return api.getAccountInfo( testAddress.public)
+  }).then(info=>{
   console.log(info);
   console.log('GetAccountInfoDone')
 })
-
-/*
-Output info:
-
-{
-  sequence: 6,
-  xrpBalance: '435.16216',
-  ownerCount: 1,
-  previousAffectingTransactionID: '9DC318489DB9A900A40CBE26EF84A1E6A11DB4E94CECB6CE27A7DA23259BF431',
-  previousAffectingTransactionLedgerVersion: 35624580
-}
-
-*/
